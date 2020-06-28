267acf3d66f106df2ccdefb5c78e23994066db67
Imogen Todd <imo.todd@gmail.com>
2020-06-28 19:34:55 -0400

    starts zoom feature

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const width = 1200;
-const height = 1000;
+const width = 1250;
+const height = 500;
 const padding = 40;
+let allStars = [];
+const oneConst = [];
 
 const svg = d3.select("body")
     .append("svg")
     .attr("height", height)
-    .attr("width", width);
+    .attr("width", width)
+    .attr("id", "svg");
 
-d3.json("constellations.json").then(function(data){
+const g = svg.append("g")
+    .attr("id", "g");
+
+async function makeChart() {
+    let data = await d3.json("constellations.json");
+    await addStars(data);
+    await mapStars(allStars);
+}
+
+async function addStars(data) {
 
     let dataset = data.features;
 
@@ -18,50 +30,8 @@ d3.json("constellations.json").then(function(data){
             zodiacOnly.push(data.features[i]);
         } else {}
     };
-
-    xScale = d3.scaleLinear()
-        .domain([
-            d3.min(zodiacOnly, function(d) {
-                return d3.min(d.geometry.coordinates[0][0]);
-            }),
-            d3.max(zodiacOnly, function(d) {
-                return d3.max(d.geometry.coordinates[0][0]);
-            }),
-        ])
-        .range([padding, width - padding]);
-
-    yScale = d3.scaleLinear()
-        .domain([
-            d3.min(zodiacOnly, function(d) {
-                return d3.min(d.geometry.coordinates[0][1]);
-            }),
-            d3.max(zodiacOnly, function(d) {
-                return d3.max(d.geometry.coordinates[0][1]);
-            }),
-        ])
-        .range([padding, height - padding]);
-
-    const collection = svg.selectAll(".collection") //<-- the outer array
-        .data(zodiacOnly)
-        .enter()
-        .append("g")
-        .attr("class", d => d.id);
-
-    collection.selectAll("point") //<-- here the nest
-        .data(d => d.geometry.coordinates[0]) //<-- the points within the inner array in that outer array
-        .enter()
-        .append("circle")
-        .attr("class", "point")
-        .attr("cx", d => xScale(d[0]))
-        .attr("cy", d => yScale(d[1]))
-        .attr("fill", "#ffffff")
-        .attr("opacity", .75)
-        .attr("r", 3)
-        .attr("stroke", "none");
     
-    let nodes = [];    
     let starID = {};
-    console.log(zodiacOnly);
 
     for (let h = 0; h < zodiacOnly.length; h ++) {
         for (let i = 0; i < zodiacOnly[h].geometry.coordinates.length; i++) {
@@ -72,33 +42,107 @@ d3.json("constellations.json").then(function(data){
                     "star": star,
                     "ident": ident
                 };
-                console.log(starID);
-                nodes.push(starID);
+                allStars.push(starID);
             }
         }
     }
+}
 
-    console.log(nodes);
+async function mapStars(nodes) {
 
-    nodes.forEach(function(d){
-        d.star[0] = xScale(d.star[0]);
-        d.star[1] = yScale(d.star[1]);
-    });
+    console.log("clicked");
+    xScale = d3.scaleLinear()
+        .domain([
+            d3.min(nodes, d => d.star[0]),
+            d3.max(nodes, d => d.star[0])
+        ])
+        .range([width - padding, padding]);
+    
+    yScale = d3.scaleLinear()
+        .domain([
+            d3.min(nodes, d => d.star[1]),
+            d3.max(nodes, d => d.star[1])
+        ])
+        .range([height-2*padding, 2*padding]);
 
-    for (let k = 0; k < nodes.length; k++) {
+    // nodes.forEach(function(d){
+    //     d.star[0] = xScale(d.star[0]);
+    //     d.star[1] = yScale(d.star[1]);
+    // });
 
-        const link = d3.linkHorizontal()({
-            source: nodes[k].star,
-            target: nodes[k+1].star
-            });
-            
-        //   Append the link to the svg element
-        svg.append('path')
-            .attr('d', link)
-            .attr('stroke', 'white')
-            .attr('fill', 'none'); 
+    g.selectAll("circle")
+        .data(nodes)
+        .enter()
+        .append("circle")
+        .attr("class", d => d.ident)
+        .attr("cx", d => xScale(d.star[0]))
+        .attr("cy", d => yScale(d.star[1]))
+        .attr("fill", "#ffffff")
+        .attr("opacity", .75)
+        .attr("r", 3)
+        .attr("stroke", "none");
+}
+
+(async () => {
+    await makeChart();
+    
+    showHighlighted();
+
+    function showHighlighted() {
+        p = document.getElementsByTagName("p");
+        for (var i = 0, len = p.length; i < len; i++) {
+            p[i].addEventListener("mouseover", (e) => highlight(e));
+            p[i].addEventListener("mouseleave", (e) => remove(e));
+            p[i].addEventListener("click", (e) => zoomIn(e));
+        }
     }
 
-    return svg.node();
+    function highlight(event) {
+        thisClass = event.currentTarget.id;
+        theseStars = document.getElementsByClassName(`${thisClass}`);
+        for (let k = 0; k < theseStars.length; k++) {
+            theseStars[k].classList.add("highlight");
+        };
+    }
 
-});
+    function remove(event) {
+        thisClass = event.currentTarget.id;
+        theseStars = document.getElementsByClassName(`${thisClass}`);
+        for (let k = 0; k < theseStars.length; k++) {
+            theseStars[k].classList.remove("highlight");        
+        };
+    }
+
+    function zoomIn(event) {
+        thisClass = event.currentTarget.id;
+        for (e=0; e < allStars.length; e++) {
+            if (allStars[e].ident == thisClass) {
+                oneConst.push(allStars[e]);
+            }
+        }
+        const median = Math.round((oneConst.length) / 2);
+        const middleStarX = xScale(oneConst[median].star[0]);
+        const middleStarY = yScale(oneConst[median].star[1]);
+        const middleStar = [middleStarX, middleStarY]; 
+        transition(middleStar);  
+    }
+    
+    const zoom = d3.zoom()
+        .on("zoom", zoomed);
+        
+    svg.call(zoom)
+    .on("wheel.zoom", null);;
+
+    function zoomed() {
+    g.attr('transform', `translate(${d3.event.transform.x},  	 ${d3.event.transform.y}) scale(${d3.event.transform.k})`);
+    }
+
+    function transition(zoomPoint) {
+        svg.transition()
+            .duration(700)
+            .call(zoom.scaleBy, 2.5, zoomPoint);
+    }
+
+
+})();
+
